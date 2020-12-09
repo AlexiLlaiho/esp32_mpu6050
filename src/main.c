0d277ddf75f7c3c61eb44e5e50763689dffb0fb8
alexillaiho <alexillaiho@gmail.com>
2020-12-09 18:22:41 +0300

    void write_file_anv(void) now working here

--- src/main.c
+++ src/main.c
@@ -32,9 +32,9 @@ esp_err_t event_handler(void *ctx, system_event_t *event)
 void app_main(void) 
 {
 	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-	xTaskCreatePinnedToCore(&vTask2, "vTask2", 2048, NULL, 5, NULL, 0);
-	// xTaskCreatePinnedToCore(&task_mpu6050, "task_mpu6050", 8192, NULL, 1, NULL, 0);
-	xTaskCreatePinnedToCore(&task_write_file, "task_write_file", 8192, NULL, 3, NULL, 0);
+	xTaskCreatePinnedToCore(&vTask2, "vTask2", 1024, NULL, 5, NULL, 0);
+	xTaskCreatePinnedToCore(&task_mpu6050, "task_mpu6050", 8192, NULL, 1, NULL, 0);
+	//xTaskCreatePinnedToCore(&task_write_file, "task_write_file", 8192, NULL, 3, NULL, 0);
 	// xTaskCreatePinnedToCore(&mcpwm_example_servo_control, "mcpwm_example_servo_control", 2048, NULL, 4, NULL, 0);
 
 	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
